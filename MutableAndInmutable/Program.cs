b8504cba8d9579789c4d7f1a177db0520dc39257
Alejandro <33105215+lalejogg@users.noreply.github.com>
2020-09-04 17:38:37 -0500

    Add files via upload

---
+++ MutableAndInmutable/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace MutableAndInmutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int k = 0; k < 10; k++)
+            {
+                DateTime dtini = DateTime.Now;
+
+                string sStringBasico = string.Empty;
+
+
+
+                //Inmutable
+                for (int i = 0; i < 10000; i++)
+                {
+                    sStringBasico += "Modified ";
+                }
+
+
+
+
+                DateTime dtfin = DateTime.Now;
+
+                int difS = dtfin.Second - dtini.Second;
+                int difmS = dtfin.Millisecond - dtini.Millisecond;
+
+                Console.WriteLine("Inmutable time: " + difmS.ToString() + " - " + difS.ToString());
+            }
+
+
+            for (int k = 0;k < 10; k++)
+            {
+                DateTime dtini = DateTime.Now;
+
+                StringBuilder strB = new StringBuilder();
+
+                //Mutable
+                for (int i = 0; i < 10000; i++)
+                {
+                    strB.Append("Modified ");
+                }
+
+                DateTime dtfin = DateTime.Now;
+
+                int difS = dtfin.Second - dtini.Second;
+                int difmS = dtfin.Millisecond - dtini.Millisecond;
+
+                Console.WriteLine("Mutable time: " + difmS.ToString() + " - " + difS.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
